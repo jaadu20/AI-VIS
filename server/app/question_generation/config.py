aa7e4b4ba3009fac63ee290e90c659830741888c
jaadu20 <jawadfarid383@gmail.com>
2025-03-19 13:37:38 +0500

    change model

--- server/app/question_generation/config.py
+++ server/app/question_generation/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     db_url: str = "postgresql://postgres:52-IsB_24@localhost:5432/aivisdb"
-    model_name: str = "t5-base"
+    model_name: str = "google-t5/t5-large"
     initial_questions: int = 3
     max_questions: int = 10
 
--- server/app/question_generation/model_test.py
+++ server/app/question_generation/model_test.py
@@ -1,25 +1,9 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-# Use the correct model identifier
-tokenizer = AutoTokenizer.from_pretrained("t5-large")
-model = AutoModelForSeq2SeqLM.from_pretrained("t5-large")
-
 import torch
-
-def generate_interview_question(job_description):
-    prompt = f"Generate an easy-level interview question based on the following job description: {job_description}"
-    
-    # Tokenize the input text
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    
-    # Generate output
-    output_ids = model.generate(input_ids, max_length=50, num_return_sequences=1)
-    
-    # Decode the output
-    question = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    
-    return question
-
-job_desc = "Software Engineer responsible for developing web applications using Python and Django."
-question = generate_interview_question(job_desc)
-print("Generated Interview Question:", question)
+from transformers import BitsAndBytesConfig, pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+model = AutoModelForSeq2SeqLM.from_pretrained("meta-llama/Meta-Llama-3-8B")
+input_text = "give technical question related to applied python"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
