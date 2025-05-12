179c83d098a7a494cb7aa964ad866d6ac21488fa
jaadu20 <jawadfarid383@gmail.com>
2025-05-12 22:14:11 +0500

    serializer

---
+++ server/app/interview_applications/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Application
+from jobs.serializers import JobSerializer
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    job_details = JobSerializer(source='job', read_only=True)
+    
+    class Meta:
+        model = Application
+        fields = '__all__'
+        read_only_fields = ['id', 'user', 'status', 'match_score', 'missing_skills', 'feedback']
+
+class ApplicationCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = ['job', 'cv']
+        
+class EligibilityCheckSerializer(serializers.Serializer):
+    cv = serializers.FileField()
+    job = serializers.UUIDField()
+    
+class ScheduleInterviewSerializer(serializers.Serializer):
+    cv = serializers.FileField()
+    job = serializers.UUIDField()
+    interview_date = serializers.DateField()
+    interview_time = serializers.CharField()
