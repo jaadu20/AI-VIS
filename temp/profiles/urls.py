1c9edb9200b0941a3fd87ed218dfe560f25f6d29
jaadu20 <jawadfarid383@gmail.com>
2025-06-06 18:59:55 +0500

    fyp

--- temp/profiles/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CandidateProfileView
-
-urlpatterns = [
-    path('candidate/profile/', CandidateProfileView.as_view(), name='candidate-profile'),
-]
